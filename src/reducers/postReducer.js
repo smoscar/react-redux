41855ad5490f3771a5dd0cb9eb8a05a68eed7a59
Oscar Merlos <oscar.merlos.arias@gmail.com>
2018-07-23 22:25:54 -0700

    Create post action created

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -8,10 +8,14 @@ const initialState = {
 export default function(state = initialState, action){
 	switch( action.type ){
 		case FETCH_POSTS:
-			console.log('fetch reducer')
 			return {
 				...state,
 				items: action.payload
+			}
+		case NEW_POSTS:
+			return {
+				...state,
+				item: action.payload
 			}
 		default:
 			return state;
