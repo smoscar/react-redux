b5969d4ff260f701ef6b3ee00e743b93d873f044
Oscar Merlos <oscar.merlos.arias@gmail.com>
2018-07-23 19:30:30 -0700

    React finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Posts from './components/posts';
+import PostForm from './components/postForm';
+
 class App extends Component {
   render() {
     return (
@@ -10,9 +13,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+				<PostForm />
+				<hr />
+        <Posts />
       </div>
     );
   }
